1bbd341edf66e0913195155e1bfae006cce34c93
jangismarifet <jangismarifet@gmail.com>
2022-06-05 22:47:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+
+function clickAlert() {
+    alert('I was clicked!');
 }
+
+function addingEventListener() {
+    input.addEventListener('click', clickAlert);
+}
+
